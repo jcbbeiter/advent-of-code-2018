5446afdd157d3344b4a96cd974f4ae586a57e7b7
Jacob Beiter <jcb.beiter@gmail.com>
2018-12-03 14:18:10 -0500

    clean up input reading in day 01

--- day_01.py
+++ day_01.py
@@ -1,30 +1,23 @@
 import sys
+from itertools import cycle
 
-def part_one():
+def part_one(freqs):
+    print(sum(freqs))
 
-    result = 0
-
-    with open('input/input_01.txt') as f:
-        for line in f.readlines():
-            sign = 1 if line[0] == '+' else -1
-            result += sign*int(line[1:])
-
-    print(result)
-
-def part_two():
+def part_two(freqs):
     total = 0
     seen = set()
 
-    lines = open('input/input_01.txt').readlines()
-    while True:
-        for line in lines:
-            sign = 1 if line[0] == '+' else -1
-            total += sign*int(line[1:])
-            if total in seen:
-                print(total)
-                return
-            seen.add(total)
+    for freq in cycle(freqs):
+        total += freq
+        if total in seen:
+            print(total)
+            return
+        seen.add(total)
 
 if __name__ == '__main__':
-    part_one()
-    part_two()
+
+    freqs = [(1 if line[0] == '+' else -1) * int(line[1:]) 
+                for line in open('input/input_01.txt').readlines()]
+    part_one(freqs)
+    part_two(freqs)
